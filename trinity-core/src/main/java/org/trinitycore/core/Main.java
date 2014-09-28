7d016fb977fb66053056f76270dbdfe2f77f8f52
John-r <skryn01@gmail.com>
2014-09-28 13:04:26 +0200

    Reviewing starting of the plugin

--- trinity-core/src/main/java/org/trinitycore/core/Main.java
+++ trinity-core/src/main/java/org/trinitycore/core/Main.java
@@ -1,16 +1,40 @@
 package org.trinitycore.core;
 
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.trinity.api.database.DatabaseService;
+import org.trinitycore.database.DatabaseModule;
+
+import java.sql.SQLException;
 
 /**
  * Managed by romain on 28/09/2014.
  */
 public class Main extends JavaPlugin {
-    public void onEnable() {
+    @Inject
+    DatabaseService database;
 
+    public void onEnable() {
+        getLogger().info("initializing guice injector..");
+        Guice.createInjector(new MainModule(this), new DatabaseModule());
+
+        getLogger().info("initializing database..");
+        try {
+            database.start();
+        } catch (SQLException e) {
+            getLogger().warning("can't connect to database, please check your sql account");
+            getLogger().warning("can't run, reload the server");
+        }
+
+        getLogger().info("plugin is now enabled");
     }
 
     public void onDisable() {
+        getLogger().info("stopping database..");
+        database.stop();
 
+        getLogger().info("plugin is now disabled");
     }
 }
