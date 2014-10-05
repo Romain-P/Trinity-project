728ccce2f4c5d8afd5a84dd34f1319673e0e2f9c
John-r <skryn01@gmail.com>
2014-10-05 20:53:31 +0200

    Creating TrinityHook, adding default plugin logger

---
+++ trinity-core/src/main/java/org/trinitycore/hooks/TrinityHook.java
@@ -0,0 +1,20 @@
+package org.trinitycore.hooks;
+
+import com.google.inject.Inject;
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+/**
+ * Managed by romain on 28/09/2014.
+ */
+public class TrinityHook {
+    @Getter
+    private final Logger logger;
+
+    @Inject
+    public TrinityHook(JavaPlugin plugin) {
+        this.logger = plugin.getLogger();
+    }
+}
