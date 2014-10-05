b960c23945868b29ac5e6c9a350960e3d80cf093
John-r <skryn01@gmail.com>
2014-10-05 20:57:18 +0200

    Removing hook binds from MainModule

--- trinity-core/src/main/java/org/trinitycore/core/MainModule.java
+++ trinity-core/src/main/java/org/trinitycore/core/MainModule.java
@@ -1,7 +1,16 @@
 package org.trinitycore.core;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.trinitycore.hooks.LevelHook;
+import org.trinitycore.hooks.TrinityHook;
+
+import java.io.File;
+import java.util.logging.Logger;
 
 /**
  * Managed by romain on 28/09/2014.
@@ -9,13 +18,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class MainModule extends AbstractModule {
     private JavaPlugin plugin;
 
+    @Inject TrinityHook trinity;
+
     public MainModule(JavaPlugin plugin) {
         this.plugin = plugin;
     }
     
     @Override
     protected void configure() {
+        Config config = ConfigFactory.parseFile(new File("config.conf"));
+        if(config.isEmpty()) {
+            trinity.getLogger().warning("Failed to load config.conf");
+            System.exit(1);
+        }
+        bind(Config.class).toInstance(config);
         bind(JavaPlugin.class).toInstance(plugin);
-        bind(TrinityHook.class).asEagerSingleton();
     }
 }
