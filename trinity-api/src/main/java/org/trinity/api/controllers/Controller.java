8eec26fd4727f828c869cb7a627f6e63ab6e5ca6
John-r <skryn01@gmail.com>
2014-10-08 16:28:52 +0200

    Creating Controller interface

--- trinity-api/src/main/java/org/trinity/api/controllers/Controller.java
+++ trinity-api/src/main/java/org/trinity/api/controllers/Controller.java
@@ -1,12 +1,13 @@
 package org.trinity.api.controllers;
 
 import com.google.common.collect.ImmutableList;
-import org.bukkit.event.Listener;
+import org.trinity.api.bukkit.ImprovedListener;
 
 /**
  * Managed by romain on 07/10/2014.
  */
 public interface Controller {
-    ImmutableList<Listener> getListeners();
+    ImmutableList<ImprovedListener> getListeners();
     void start();
+    void stop();
 }
