ea1dfef910ec930c527c04b670949f14c3cf0262
John-r <skryn01@gmail.com>
2014-10-08 16:34:12 +0200

    Adding new fields to the trinity default hook

--- trinity-core/src/main/java/org/trinitycore/backend/hooks/TrinityHook.java
+++ trinity-core/src/main/java/org/trinitycore/backend/hooks/TrinityHook.java
@@ -2,8 +2,16 @@ package org.trinitycore.backend.hooks;
 
 import com.google.inject.Inject;
 import lombok.Getter;
+import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.trinity.api.database.DaoQueryManager;
+import org.trinity.api.database.DatabaseService;
+import org.trinitycore.database.managers.ClientManager;
+import org.trinitycore.database.managers.LevelManager;
+import org.trinitycore.frontend.entities.Client;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 /**
@@ -12,9 +20,21 @@ import java.util.logging.Logger;
 public class TrinityHook {
     @Getter
     private final Logger logger;
+    @Getter
+    private final Server server;
+    @Getter
+    private final Map<String, Client> clients;
+    @Getter
+    private final DaoQueryManager<Client> clientManager;
+    @Getter
+    private final DaoQueryManager<LevelHook.Level> levelManager;
 
     @Inject
-    public TrinityHook(JavaPlugin plugin) {
+    public TrinityHook(JavaPlugin plugin, DatabaseService database) {
         this.logger = plugin.getLogger();
+        this.server = plugin.getServer();
+        this.clients = new HashMap<>();
+        this.clientManager = (DaoQueryManager<Client>) database.getQueryManagers().get(ClientManager.class);
+        this.levelManager = (DaoQueryManager<LevelHook.Level>) database.getQueryManagers().get(LevelManager.class);
     }
 }
